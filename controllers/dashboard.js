75ce939313e443009ab1c399f491c088e44de3bd
csherlock4 <csherlock4@ivytech.edu>
2024-03-28 12:14:04 -0400

    added wishlist feature, log out to admin dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,29 +1,38 @@
-const { getUser } = require('../models/users');
-const { Bloom } = require('../models/blooms'); // Import the Bloom model
+const { User } = require('../models/users'); 
+const { Bloom } = require('../models/blooms'); 
 
 const dashboardController = {
     get: async (req, res) => {
-        const user = req.session?.user;
-        if (!user) {
+        const userSession = req.session?.user;
+        if (!userSession) {
             return res.redirect('/login');
         }
 
-        // Fetch all blooms if the user is an admin
-        if (user.role === 'admin') {
-            try {
+        try {
+            if (userSession.role === 'admin') {
+                // Fetch all blooms if the user is an admin
                 const blooms = await Bloom.find({}).lean();
-                return res.render('dashboard', { user, isAdmin: true, blooms });
-            } catch (error) {
-                console.error('Error fetching blooms: ', error);
-                return res.status(500).send('Error fetching blooms');
-            }
-        }
+                return res.render('dashboard', { user: userSession, isAdmin: true, blooms });
+            } else {
+                // For a non-admin user
+                const userWithWishList = await User.findById(userSession._id)
+                                                   .populate('wishList')
+                                                   .lean();
 
-        // If not an admin, just render the user's dashboard
-        // Fetch user's blooms or other relevant data
-        const newUser = await getUser(null, user.email);
-        req.session.user.blooms = newUser.blooms;
-        return res.render('dashboard', { user, isAdmin: false });
+                
+                req.session.user = { ...userSession, ...userWithWishList };
+
+                // Render the dashboard for a regular user with their wishlist
+                return res.render('dashboard', {
+                    user: req.session.user,
+                    isAdmin: false,
+                    blooms: userWithWishList.wishList, 
+                });
+            }
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+            return res.status(500).send('An error occurred');
+        }
     }
 };
 
