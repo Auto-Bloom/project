48c6de4ade076ea8cc97cc46814d8a908de1ef16
csherlock4 <csherlock4@ivytech.edu>
2024-03-28 13:49:08 -0400

    moved update and delete functions

--- routes/blooms.js
+++ routes/blooms.js
@@ -1,12 +1,15 @@
-// Change the require statement to import the whole controller object
+const express = require('express');
 const bloomsController = require('../controllers/blooms');
-const bloomsRouter = require('express').Router();
+const bloomsRouter = express.Router();
 
-// Use the controller's methods when defining routes
-bloomsRouter.get('/:bloomId?', bloomsController.get);
+bloomsRouter.get('/addBlooms', (req, res) => {
+
+  res.render('addBlooms');
+});
+bloomsRouter.get('/update/:bloomId', bloomsController.updateForm);
+bloomsRouter.get('/:bloomId', bloomsController.get);
 bloomsRouter.post('/', bloomsController.post);
 bloomsRouter.put('/:bloomId', bloomsController.put);
 bloomsRouter.delete('/:bloomId', bloomsController.del);
 
 module.exports = bloomsRouter;
-
